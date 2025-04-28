575c7ebaee7417d0f556942ac9dbcf31d10d275d
rsuntk <rsuntk@yukiprjkt.my.id>
2025-04-28 20:26:04 +0000

    fs/proc: fmt lineage hide codes
    * Intentional?
    
    Signed-off-by: rsuntk <rsuntk@yukiprjkt.my.id>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -384,14 +384,14 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma)
         struct dentry *dentry = file->f_path.dentry;
         if (dentry) {
         	const char *path = (const char *)dentry->d_name.name; 
-            	if (strstr(path, "lineage")) { 
-	  	start = vma->vm_start;
-		end = vma->vm_end;
-		show_vma_header_prefix_fake(m, start, end, flags, pgoff, dev, ino);
+            if (strstr(path, "lineage")) {
+				start = vma->vm_start;
+				end = vma->vm_end;
+				show_vma_header_prefix_fake(m, start, end, flags, pgoff, dev, ino);
             	name = "/system/framework/framework-res.apk";
-		goto done;
-            	 	}
-            	}
+				goto done;
+         	}
+        }
 	}
 
 	start = vma->vm_start;
