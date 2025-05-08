Three-HeadedCamel/lav419

UPSTREAM: mm: multi-gen LRU: fix crash during cgroup migration lru_gen_migrate_mm() assumes lru_gen_add_mm() runs prior to itself.  This isn't true for the following scenario:
    CPU 1                         CPU 2

  clone()
    cgroup_can_fork()
                                cgroup_procs_write()
    cgroup_post_fork()
                                  task_lock()
                                  lru_gen_migrate_mm()
                                  task_unlock()
    task_lock()
    lru_gen_add_mm()
    task_unlock()

And when the above happens, kernel crashes because of linked list
corruption (mm_struct->lru_gen.list).

Bug: 254441685
Link: https://lore.kernel.org/r/20230115134651.30028-1-msizanoen@qtmlabs.xyz/
Link: https://lkml.kernel.org/r/20230116034405.2960276-1-yuzhao@google.com
Fixes: bd74fdaea146 ("mm: multi-gen LRU: support page table walks")
Signed-off-by: Yu Zhao <yuzhao@google.com>
Reported-by: msizanoen <msizanoen@qtmlabs.xyz>
Tested-by: msizanoen <msizanoen@qtmlabs.xyz>
Cc: <stable@vger.kernel.org>	[6.1+]
Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
(cherry picked from commit de08eaa6156405f2e9369f06ba5afae0e4ab3b62)
Signed-off-by: Lee Jones <joneslee@google.com>
Change-Id: I2c6c97b0f7d1cc4f9f40292414d5cb5ef8b89ad2

Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

mm/vmscan.c
@@ -2771,13 +2771,16 @@ void lru_gen_migrate_mm(struct mm_struct *mm)
 	if (mem_cgroup_disabled())
 		return;
 
+	/* migration can happen before addition */
+ 	if (!mm->lru_gen.memcg)
+ 		return;
+
 	rcu_read_lock();
 	memcg = mem_cgroup_from_task(mm->owner);
 	rcu_read_unlock();
 	if (memcg == mm->lru_gen.memcg)
 		return;
 
-	VM_BUG_ON_MM(!mm->lru_gen.memcg, mm);
 	VM_BUG_ON_MM(list_empty(&mm->lru_gen.list), mm);
 
 	lru_gen_del_mm(mm);

rsuntk/x01bd-krenol-4.19

mm/vmscan.c
@@ -2926,13 +2926,16 @@ void lru_gen_migrate_mm(struct mm_struct *mm)
 	if (mem_cgroup_disabled())
 		return;
 
+	/* migration can happen before addition */
+ 	if (!mm->lrugen.memcg)
+ 		return;
+
 	rcu_read_lock();
 	memcg = mem_cgroup_from_task(mm->owner);
 	rcu_read_unlock();
 	if (memcg == mm->lrugen.memcg)
 		return;
 
-	VM_BUG_ON_MM(!mm->lrugen.memcg, mm);
 	VM_BUG_ON_MM(list_empty(&mm->lrugen.list), mm);
 
 	lru_gen_del_mm(mm);